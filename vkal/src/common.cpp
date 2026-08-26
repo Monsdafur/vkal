e6e3a3bf00807c28f6e9c95e93fec4a8bc30098d
Monsdafur <dqhoa06@gmail.com>
2026-08-26 07:40:12 +0700

    added size conversion helpers

--- vkal/src/common.cpp
+++ vkal/src/common.cpp
@@ -8,4 +8,16 @@ void debug(const std::string& message) {
     std::println("INFO   | {}", message);
 }
 
+vk::DeviceSize kilobytes(vk::DeviceSize value) {
+    return value * 1024;
+}
+
+vk::DeviceSize megabytes(vk::DeviceSize value) {
+    return kilobytes(value) * 1024;
+}
+
+vk::DeviceSize gigabytes(vk::DeviceSize value) {
+    return megabytes(value) * 1024;
+}
+
 } // namespace vkal
--- vkal/src/common.hpp
+++ vkal/src/common.hpp
@@ -1,7 +1,15 @@
+#include <vulkan/vulkan.hpp>
+
 #include <string>
 
 namespace vkal {
 
 void debug(const std::string& message);
+
+vk::DeviceSize kilobytes(vk::DeviceSize value);
+
+vk::DeviceSize megabytes(vk::DeviceSize value);
+
+vk::DeviceSize gigabytes(vk::DeviceSize value);
 
 } // namespace vkal
