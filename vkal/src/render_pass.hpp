456fc80efd1a4bd6b1e81f67814d3e37b0cbecc5
Monsdafur <dqhoa06@gmail.com>
2026-09-06 01:03:28 +0700

    added render pass

---
+++ vkal/src/render_pass.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "buffer.hpp"
+#include "descriptor_set.hpp"
+#include "image.hpp"
+#include "pipeline.hpp"
+
+#include <unordered_map>
+
+namespace vkal {
+
+class RenderPass {
+  public:
+    virtual ~RenderPass() = default;
+    virtual void setup_metadata(
+        const std::unordered_map<std::string, std::reference_wrapper<Buffer>>&,
+        const std::unordered_map<std::string, std::reference_wrapper<Image>>&,
+        const std::unordered_map<std::string, std::reference_wrapper<vk::RenderingAttachmentInfo>>&,
+        std::optional<std::reference_wrapper<Pipeline>>,
+        std::optional<std::reference_wrapper<DescriptorSet>>) {
+    }
+    virtual void render(vk::CommandBuffer) {
+    }
+};
+
+}; // namespace vkal
